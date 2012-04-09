c36435e35268d31055f3c73c045bd65642ee4c5d
Lucas Chi <chi.lucas@gmail.com>
2012-04-09 15:20:40 -0400

    prepared for presentation

--- cookbook_str.rb
+++ cookbook_str.rb
@@ -1,9 +1,10 @@
+#!/usr/bin/env ruby
 require './markov.rb'
 
 m = Markov.new
 
-loop do
-  seed = gets.strip
-  len = gets.to_i
-  puts m.generate_paragraph("cookbooks", seed, len, 3)
-end
+seed = ARGV[0] ? ARGV[0] : 'The'
+order = ARGV[1] ? ARGV[1].to_i : 3
+len = ARGV[2] ? ARGV[2].to_i : 40
+
+puts m.generate_paragraph("cookbooks", seed, len, order)
